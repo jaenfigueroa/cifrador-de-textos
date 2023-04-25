9fde1aed04263e6dbc7b63a8e5b1f6b46f4f2f3c
jaen <jaenfigueroa03042000@gmail.com>
2023-04-25 14:35:19 -0500

    refactor code

--- main.js
+++ main.js
@@ -1,54 +1,27 @@
-const input = document.querySelector('#textarea1')
-const results = document.querySelector('#textarea2')
-const warningSection = document.querySelector('#warningSection')
-const resultsSection = document.querySelector('#resultsSection')
+const textarea = document.getElementById('textarea1')
+const resultado = document.getElementById('textarea2')
+const seccionVacio = document.getElementById('warningSection')
+const seccionResultado = document.getElementById('resultsSection')
+const DICCIONARIO = Object.freeze({ e: 'enter', i: 'imes', o: 'ober', a: 'ai', u: 'ufat' })
 
-const DICTIONARY = {
-  e: 'enter',
-  i: 'imes',
-  o: 'ober',
-  a: 'ai',
-  u: 'ufat'
-}
-
-// ESCUCHAR EL EVENTO DE CLICK
-const handleClick = (type) => {
-  const inputValue = input.value
-
-  const newText = encryptDecrypt(inputValue, type)
-  showResults(newText)
-}
-
-// ENCRIPTAR O DESENCRIPTAR TEXTO
-const encryptDecrypt = (text, type) => {
-  for (const key in DICTIONARY) {
-
-    if (type === 'encrypt') {
-      text = text.replace(new RegExp(key, 'g'), DICTIONARY[key])
-    } else {
-      text = text.replace(new RegExp(DICTIONARY[key], 'g'), key)
-    }
+const cifrarDecifrar = (text, type) => {
+  for (const key in DICCIONARIO) {
+    type === 'encrypt' && (text = text.replaceAll(key, DICCIONARIO[key]))
+    type === 'decrypt' && (text = text.replaceAll(DICCIONARIO[key], key))
   }
   return text
 }
 
-// MOSTRAR RESULTADOS, ALTERNAR ENTRE LAS SECCIONES
-const showResults = (text) => {
-  results.value = text
-
-  warningSection.classList.toggle('novisible', !!text)
-  resultsSection.classList.toggle('novisible', !text)
+const mostrarResultados = (text) => {
+  resultado.value = text
+  seccionVacio.classList.toggle('novisible', !!text)
+  seccionResultado.classList.toggle('novisible', !text)
 }
 
-// COPIAR TEXTO
-const copyText = () => {
-  navigator.clipboard.writeText(results.value)
+const copiarTexto = () => navigator.clipboard.writeText(resultado.value)
 
-  // ** cambiar estilos del boton
-  copyButton.innerText = 'Texto copiado :D'
-  copyButton.classList.add('button--copy')
-  setTimeout(() => {
-    copyButton.innerText = 'Copiar'
-    copyButton.classList.remove('button--copy')
-  }, 1000)
+const limpiarTexto = (e) => {
+  textarea.value = e.target.value.replaceAll(/[^a-z\s]+/g, '')
 }
+
+const handleClick = (type) => mostrarResultados(cifrarDecifrar(textarea.value, type))
