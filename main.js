309a8ef5b51dbafe97923afad262212e72ca1fd5
jaen <jaenfigueroa03042000@gmail.com>
2023-06-01 19:12:46 -0500

    :recycle: refactor: improve code

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const DICCIONARIO = Object.freeze({ e: 'enter', i: 'imes', o: 'ober', a: 'ai', u: 'ufat' })
 const seccionVacio = document.getElementById('warningSection')
 const seccionResultado = document.getElementById('resultsSection')
-const resultado = document.getElementById('resultado')
 const texto = document.getElementById('texto')
+const resultado = document.getElementById('resultado')
 
 const cifrarDecifrar = (text, type) => {
   for (const key in DICCIONARIO) {
-    type === 'encrypt' && (text = text.replaceAll(key, DICCIONARIO[key]))
-    type === 'decrypt' && (text = text.replaceAll(DICCIONARIO[key], key))
+    type === 'cifrar' && (text = text.replaceAll(key, DICCIONARIO[key]))
+    type === 'decifrar' && (text = text.replaceAll(DICCIONARIO[key], key))
   }
   return text
 }
 
 const mostrarResultado = (text) => {
-  resultado.textContent = text
-  seccionVacio.classList.toggle('novisible', !!text)
+  resultado.value = text
+  seccionVacio.classList.toggle('novisible', text)
   seccionResultado.classList.toggle('novisible', !text)
 }
 
-const copiarResultado = () => navigator.clipboard.writeText(resultado.textContent)
+const copiarResultado = () => navigator.clipboard.writeText(resultado.value)
 
 const limpiarTexto = (e) => {
   texto.value = e.target.value.replaceAll(/[^a-z\s]+/g, '')
